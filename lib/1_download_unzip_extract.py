6b7dc915c63b22284b9ea19b43505fa7969fd63c
jcarhart1 <carhart77@gmail.com>
2021-04-10 17:21:53 -0400

    Update 1_download_unzip_extract.py

---
+++ lib/1_download_unzip_extract.py
@@ -0,0 +1,13 @@
+
+import urllib3 as urllib
+import zipfile2 as zipfile
+
+# download and unzip
+url = 'https://api.pharmgkb.org/v1/download/file/data/clinicalAnnotations.zip'
+filehandle, _ = urllib.urlretrieve(url)
+zip_file_object = zipfile.ZipFile(filehandle, 'r')
+first_file = zip_file_object.namelist()[0]
+file = zip_file_object.open(first_file)
+content = file.read()
+
+# extract to local (db next iteration)
